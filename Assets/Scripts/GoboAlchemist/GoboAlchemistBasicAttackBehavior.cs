781aa8862b4fe3c7915b769c69e19309b04e30f1
31467 <nils.paassen@hsrw.org>
2023-06-24 16:06:18 +0200

    Finished GoboAlchemist

--- Assets/Scripts/GoboAlchemist/GoboAlchemistBasicAttackBehavior.cs
+++ Assets/Scripts/GoboAlchemist/GoboAlchemistBasicAttackBehavior.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class GoboAlchemistBasicAttackBehavior : MonoBehaviour
 {
+    [SerializeField]
+    GameObject explosion;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>().AddForce(new Vector3(transform.forward.x*2,2,0));
+        GetComponent<Rigidbody>().AddForce(new Vector3(transform.forward.x*12,7,0), ForceMode.Impulse);
     }
 
     void Update()
     {
         transform.Rotate(new Vector3(0.1f,0.1f,0.1f));
+    }
+
+    void OnTriggerEnter(Collider e)
+    {
+        if (e.gameObject.tag == "Ground")
+        {
+            Instantiate(explosion,transform.position - new Vector3(0,1.5f,0),new Quaternion(0,0,0,1));
+            Destroy(this.gameObject);
+        }
     }
 }
