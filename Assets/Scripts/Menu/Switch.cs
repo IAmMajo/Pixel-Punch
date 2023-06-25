a821a70560211f81eb37b8e204724ffc3d346f8f
31467 <nils.paassen@hsrw.org>
2023-06-25 19:29:54 +0200

    More sorrting, setting up switch scrip for sprites

--- Assets/Scripts/Menu/Switch.cs
+++ Assets/Scripts/Menu/Switch.cs
@@ -6,28 +6,21 @@ using UnityEngine.UI;
 public class Switch : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject[] Gobos;
-    public GameObject[] Texts;
+    public Sprite[] Gobos;
+    public Sprite[] Texts;
     int index;
+
+    Image preview;
+    [SerializeField]
+    GameObject textArea;
+    Image textPreview;
     void Start()
     {
         index = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (index > 2)
-            index = 2;
-
-        if (index < 0)
-            index = 0;
-
-        if (index == 0)
-        {
-            Gobos[0].gameObject.SetActive(true);
-            Texts[0].gameObject.SetActive(true);
-        }
+        preview = GetComponent<Image>();
+        preview.sprite = Gobos[index];
+        textPreview = textArea.GetComponent<Image>();
+        textPreview.sprite = Texts[index];
     }
 
     public void Next()
@@ -41,15 +34,8 @@ public class Switch : MonoBehaviour
             index = 0;
         }
 
-
-        for (int i = 0; i < Gobos.Length; i++)
-        {
-            Gobos[i].gameObject.SetActive(false);
-            Gobos[index].gameObject.SetActive(true);
-            Texts[i].gameObject.SetActive(false);
-            Texts[index].gameObject.SetActive(true);
-        }
-        Debug.Log(index);
+        preview.sprite = Gobos[index];
+        textPreview.sprite = Texts[index];
     }
 
     public void Previous()
@@ -60,17 +46,10 @@ public class Switch : MonoBehaviour
         }
         else
         {
-            index = Gobos.Length-1;
+            index = Gobos.Length - 1;
         }
-        
 
-        for (int i = 0; i < Gobos.Length; i++)
-        {
-            Gobos[i].gameObject.SetActive(false);
-            Gobos[index].gameObject.SetActive(true);
-            Texts[i].gameObject.SetActive(false);
-            Texts[index].gameObject.SetActive(true);
-        }
-        Debug.Log(index);
+        preview.sprite = Gobos[index];
+        textPreview.sprite = Texts[index];
     }
 }
