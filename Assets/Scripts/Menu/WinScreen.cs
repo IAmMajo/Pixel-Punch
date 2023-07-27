49e8e8ee7afc3b1654487038d38ec43f0fa02cca
31467 <nils.paassen@hsrw.org>
2023-07-27 19:59:28 +0200

    Fixed Pause Menu

---
+++ Assets/Scripts/Menu/WinScreen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinScreen : MonoBehaviour
+{
+    [SerializeField] 
+    Image victor;
+    [SerializeField] 
+    Image looser;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
